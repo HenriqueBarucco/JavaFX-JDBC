5e81f58c8a54ee0673a916bd7c420fdf5a6a088b
Henrique Barucco <henrique_barucco@hotmail.com>
2022-07-19 21:35:15 -0300

    About view

--- src/main/java/com/example/workshopjavafxjdbc/Main.java
+++ src/main/java/com/example/workshopjavafxjdbc/Main.java
@@ -10,6 +10,8 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application {
+
+    private static Scene mainScene;
     @Override
     public void start(Stage stage) {
         try {
@@ -19,13 +21,17 @@ public class Main extends Application {
             scrollPane.setFitToWidth(true);
             scrollPane.setFitToHeight(true);
 
-            Scene mainScene = new Scene(scrollPane);
+            mainScene = new Scene(scrollPane);
             stage.setScene(mainScene);
             stage.setTitle("Sample JavaFX application");
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static Scene getMainScene() {
+        return mainScene;
     }
 
     public static void main(String[] args) {
