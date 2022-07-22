7d6c3e1e35144f5e9b9307c25664862fd520ada6
Henrique Barucco <henrique_barucco@hotmail.com>
2022-07-21 23:09:17 -0300

    Validation exception

--- src/main/java/com/example/workshopjavafxjdbc/DepartmentFormController.java
+++ src/main/java/com/example/workshopjavafxjdbc/DepartmentFormController.java
@@ -6,6 +6,7 @@ import com.example.workshopjavafxjdbc.gui.util.Alerts;
 import com.example.workshopjavafxjdbc.gui.util.Constraints;
 import com.example.workshopjavafxjdbc.gui.util.Utils;
 import com.example.workshopjavafxjdbc.model.entities.Department;
+import com.example.workshopjavafxjdbc.model.exceptions.ValidationException;
 import com.example.workshopjavafxjdbc.model.services.DepartmentService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -16,9 +17,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class DepartmentFormController implements Initializable {
 
@@ -70,6 +69,8 @@ public class DepartmentFormController implements Initializable {
             Utils.currentStage(event).close();
         } catch (DbException e) {
             Alerts.showAlert("Error saving object", null, e.getMessage(), Alert.AlertType.ERROR);
+        } catch (ValidationException e) {
+            setErrorMessages(e.getErrors());
         }
     }
 
@@ -81,8 +82,19 @@ public class DepartmentFormController implements Initializable {
 
     private Department getFormData() {
         Department obj = new Department();
+
+        ValidationException exception = new ValidationException("Validation error");
+
         obj.setId(Utils.tryParseToInt(txtId.getText()));
+
+        if (txtName.getText() == null || txtName.getText().trim().equals("")) {
+            exception.addError("name", "Field can't be empty");
+        }
         obj.setName(txtName.getText());
+
+        if (exception.getErrors().size() > 0) {
+            throw exception;
+        }
 
         return obj;
     }
@@ -108,5 +120,13 @@ public class DepartmentFormController implements Initializable {
         }
         txtId.setText(String.valueOf(entity.getId()));
         txtName.setText(entity.getName());
+    }
+
+    private void setErrorMessages(Map<String, String> errors) {
+        Set<String> fields = errors.keySet();
+
+        if (fields.contains("name")) {
+            labelErrorName.setText(errors.get("name"));
+        }
     }
 }
---
+++ src/main/java/com/example/workshopjavafxjdbc/model/exceptions/ValidationException.java
@@ -0,0 +1,21 @@
+package com.example.workshopjavafxjdbc.model.exceptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidationException extends RuntimeException{
+
+    private Map<String, String> errors = new HashMap<>();
+
+    public ValidationException(String msg) {
+        super(msg);
+    }
+
+    public Map<String, String> getErrors() {
+        return errors;
+    }
+
+    public void addError(String fieldName, String errorMessage) {
+        errors.put(fieldName, errorMessage);
+    }
+}
